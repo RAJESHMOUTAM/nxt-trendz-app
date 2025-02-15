eb7fb186b6acfe3af63380ec6741dc2f4f661dd8
RAJESH MOUTAM <83361003+RAJESHMOUTAM@users.noreply.github.com>
2025-02-15 10:28:41 +0530

    para_update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -23,8 +23,7 @@ const Home = () => {
           />
           <p className="home-description">
             Fashion is part of the daily air and it does not quite help that it
-            changes all the time. Clothes have always been a marker of the era
-            and we are in a revolution. Your fashion makes you been seen and
+            changes all the time. Your fashion makes you been seen and
             heard that way you are. So, celebrate the seasons new and exciting
             fashion in your own way.
           </p>
